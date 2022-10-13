357344eeb0f69c9af5aaf50012c1fd5261b8f808
Bobinho100 <bernard.aire@ucalgary.ca>
2022-10-12 23:02:06 -0600

    I tried simplyfing the projects

---
+++ calorie.py
@@ -0,0 +1,9 @@
+class Calorie:
+    def __init__(self,weight, height,age, temperature):
+        self.weight = weight
+        self.height = height
+        self.age = age
+        self.temperature = temperature
+
+    def calculate(self):
+        pass
---
+++ temperature.py
@@ -0,0 +1,8 @@
+class Temperature:
+
+    def __init__(self,country,city):
+        self.country = country
+        self.city = city
+
+    def get(self):
+        pass
