867a8c11d7c2aa61cc5eb638d9424365d0eb67c9
Bobinho100 <114113147+Bobinho100@users.noreply.github.com>
2022-12-31 12:47:55 -0700

    Finished on the web scraping part for temperature

--- temperature.py
+++ temperature.py
@@ -1,3 +1,8 @@
+from selectorlib import Extractor
+
+import requests
+
+
 class Temperature:
 
     """
@@ -6,7 +11,16 @@ class Temperature:
 
     """
 
-    headers = {}
+    headers = { 
+
+        'pragma': 'no-cache',
+        'cache-control': 'no-cache',
+        'dnt': '1',
+        'upgrade-insecure-requests': '1',
+        "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
+        'accept': 'test/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8,image/web,image/apng,application',
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8'}
+
     base_url = "https://www.timeanddate.com/weather"
     yml_path = "temperature.yaml"
 
@@ -14,12 +28,12 @@ class Temperature:
         self.country = country.replace(" ","-")
         self.city = city.replace(" ","-")
 
-    def _build_url(self):
+    def build_url(self):
         """
         Builds the url string adding country and city
         """
 
-        url = self._base_url + self.country + "/" + self.city
+        url = self.base_url + "/" + self.country + "/" + self.city
         return url
 
 
@@ -27,13 +41,13 @@ class Temperature:
         """
         Extracts a value as instructed by the yaml file and returns a dictionary
         """
-
-        url = self._build_url()
+        url = self.build_url()
         extractor = Extractor.from_yaml_file(self.yml_path)
-        r = requests.get(url, headers = self.headers)
+        r = requests.get(url, headers=self.headers)
         full_content = r.text
         raw_content = extractor.extract(full_content)
         return raw_content
+        
 
     def get(self):
 
@@ -41,10 +55,15 @@ class Temperature:
         Cleans the output of _scrape
         """
 
-        scrapped_content = self._scrape()
-        return float(scrapped_content['temp'].replace("C","").strip())
+        scrapped_content = self.scrape()
+        
+    
+        return float(scrapped_content['temp'].replace("°C","").strip())
         
 
+if __name__ == "__main__":
 
-    if __name__ == "__main__":
-        
+    temperature = Temperature(country = 'usa', city= 'san-francisco')
+    print(temperature.get())
+    
+
